22dbd0ee53784ee251f9f218482a64e37cc215fb
megha <megha@DESKTOP-M50QMMU>
2020-12-17 17:06:55 +0530

    sample second commit

--- DemoApp/src/com/lti/model/Sample.java
+++ DemoApp/src/com/lti/model/Sample.java
@@ -4,4 +4,7 @@ public class Sample {
      public void show() {
     	 System.out.println("Hello World");
      }
+     public void method() {
+    	 System.out.println("Hi,how are you doing?");
+     }
 }
