899857d1180a8e821c0a0d97fc631a7ce694838f
meghanapp <31460697+meghanapp@users.noreply.github.com>
2020-12-18 13:05:32 +0530

    added a print statement

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -5,6 +5,7 @@ public class Calc {
     	 System.out.println("sum:"+(num1+num2));
      }
      public void subs(int num1,int num2) {
-    	 System.out.println("diff:"+(num1-num2));
+    	 System.out.println("differnce:"+(num1-num2));
+         System.out.println("changes made"); 
      }
 }
